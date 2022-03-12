2236c8628f1c72019222c6cbe7f5a739ce2d8be9
ACenteno0986 <alecenteno72@gmail.com>
2022-03-12 15:10:52 -0600

    codigos de pruea

--- test.py
+++ test.py
@@ -1,35 +1,81 @@
-#Importar Modulo scipy para leer y grabar audio
 from scipy.io import wavfile 
 import IPython.display as ipd
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import numpy as np
+import pyaudio
+import wave
+import sys
 from tkinter import *
+#import interfaz
 
-AudioName = "download.wav" # Archivo de Audio
+AudioName = "file.wav"
+CHUNK = 1024
 
-# Salida fs: Frecuencia de muestreo and data: Señal de audio -> int16
-fs, Audiodata = wavfile.read(AudioName)
+wf = wave.open("file.wav", 'rb')
+p = pyaudio.PyAudio()
 
-plt.rcParams['figure.figsize'] = (15, 5) # Definir el tamaño de graficas
+stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                channels=wf.getnchannels(),
+                rate=wf.getframerate(),
+                output=True)
 
-plt.plot(Audiodata) # Audiodata es un numpy array
-plt.text(0-5000, np.max(Audiodata), 'Máximo', fontsize = 16,bbox=dict(facecolor='red', alpha=0.5))
-plt.title('Señal de Audio sin valores adecuados en los ejes',size=16)
+data = wf.readframes(CHUNK)
+signal = np.frombuffer(data, dtype ="int16")
+f_rate = wf.getframerate()
 
-# declare the window
+time = np.linspace(
+        0, # start
+        len(signal) / f_rate,
+        num = len(signal)
+    )
+
+
 window = Tk()
-# set window title
-window.title("Python GUI App")
-# set window width and height
+window.title("Autrum")
 window.configure(width=500, height=300)
-# set window background color
 window.configure(bg='lightgray')
-
 canvas = FigureCanvasTkAgg(plt.figure(1), window)
 plot_widget = canvas.get_tk_widget()
-plt.figure(1).canvas.draw()
 
 
 plot_widget.grid(row=0, column=0)
+
+
+while data != b'':
+    stream.write(data)
+    data = wf.readframes(CHUNK)
+
+    signal = np.frombuffer(data, dtype ="int16")
+    f_rate = wf.getframerate()
+    time = np.linspace(
+        0, # start
+        len(signal) / f_rate,
+        num = len(signal)
+    )
+ 
+
+    plt.figure(1)
+     
+
+    plt.title("Sound Wave")
+     
+
+    plt.xlabel("Time")
+    
+    plt.plot(time, signal)
+    
+    #plt.figure(1).canvas.draw()
+
+    #plt.show()
+
+
+stream.stop_stream()
+stream.close()
+
+p.terminate()
+
+#Audiodata = wavfile.read(AudioName)
+
+
 window.mainloop()
